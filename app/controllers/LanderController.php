5f3a3c6b93cd645ede921d813be9eb2dfb2d185c
jakedavies <jgdavies24@gmail.com>
2015-02-24 00:14:37 -0800

    moved ddl, migrations folders.

--- app/controllers/LanderController.php
+++ app/controllers/LanderController.php
@@ -4,6 +4,7 @@ class LanderController extends BaseController{
     public static function hello($request,$response)
     {
         $greeting = "Hello World";
+
         Renderer::renderView('/lander/hello',['greeting'=>$greeting]);
     }
 } 
