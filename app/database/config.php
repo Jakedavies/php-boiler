0e3bec9a2745ef7f60c4f92e89e938e407f3a4f4
jakedavies <jgdavies24@gmail.com>
2015-02-24 00:01:41 -0800

    added two tables, schema updated, models created, database configs updated

--- app/database/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//store configuration information here
-	$host = '127.0.0.1';
-	$username = 'hydrauth';
-	$password = 'hydr@';
-	$database = 'hydradb';
-?>
---
+++ db/migrations/defaultdb.sql
@@ -0,0 +1,42 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ---------------------------------------------------------------------
+-- user
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255),
+    `password` VARCHAR(255),
+    `type` VARCHAR(255),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- charity
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `charity`;
+
+CREATE TABLE `charity`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `owner` INTEGER,
+    `description` TEXT,
+    `preview` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    INDEX `fi_CHARITY_USER` (`owner`),
+    CONSTRAINT `FK_CHARITY_USER`
+        FOREIGN KEY (`owner`)
+        REFERENCES `user` (`id`)
+) ENGINE=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
